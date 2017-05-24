4bc1f73f7cd100bc5c13fa46685e6821005293dc
tomfriwel <976932447@qq.com>
2017-05-24 15:29:51 +0800

    login ui

--- MailCore2Example/ViewController.m
+++ MailCore2Example/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MailCore/MailCore.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) MCOIMAPSession *imapSession;
 
 @end
 
@@ -16,13 +19,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
