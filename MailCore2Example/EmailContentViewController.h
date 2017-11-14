190906129cdcad41e79aa4b2fd89fcfb13d2fed5
tomfriwel <976932447@qq.com>
2017-11-14 18:09:44 +0800

    prepare to view email content

---
+++ MailCore2Example/EmailContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmailContentViewController.h
+//  MailCore2Example
+//
+//  Created by tomfriwel on 14/11/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmailContentViewController : UIViewController
+
+@end
---
+++ MailCore2Example/Views/EmailTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  EmailTableViewCell.m
+//  MailCore2Example
+//
+//  Created by tomfriwel on 14/11/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import "EmailTableViewCell.h"
+
+@implementation EmailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
