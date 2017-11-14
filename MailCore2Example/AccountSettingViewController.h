32c538ea2169b6eeaced500d3c4c421bbb5d8d96
tomfriwel <976932447@qq.com>
2017-11-14 14:42:58 +0800

    clean

---
+++ MailCore2Example/AccountSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountSettingViewController.h
+//  MailCore2Example
+//
+//  Created by tomfriwel on 14/11/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountSettingViewController : UIViewController
+
+@end
